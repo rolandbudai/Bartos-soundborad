70509b14d1e182e95ef919b623c72b211528d34b
rolandbudai <46297717+rolandbudai@users.noreply.github.com>
2019-08-04 18:22:19 +0200

    small update in js and css files

--- js/bartos.js
+++ js/bartos.js
@@ -1,5 +1,3 @@
-let playing = false
-
 const sound1 = new Audio("sound/meo.mp3")
 const sound2 = new Audio("sound/szamuraj.mp3")
 const sound3 = new Audio("sound/harang.mp3")
@@ -7,11 +5,5 @@ const sound4 = new Audio("sound/so.mp3")
 const sound5 = new Audio("sound/csisztambul.mp3")
 
 function playAudio(sound) {
-    if(playing ===false){
     sound.play()
-    playing = true
-    } else {
-        sound.pause()
-        playing = false
-    }
-}
+    } 
